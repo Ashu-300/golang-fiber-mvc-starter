5e9c16e3f0be634bace8ca9e49cc77aad8656bf2
Ashu <ashutoshnigam300@gmail.com>
2025-09-02 18:10:50 +0530

    register created

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -16,6 +16,34 @@ import (
 
 const SecretKey = "secret"
 
+func Register(c *fiber.Ctx) error {
+    var data map[string]string
+
+    if err := c.BodyParser(&data); err != nil {
+        return err
+    }
+
+    password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+
+    user := models.User{
+        ID:       primitive.NewObjectID(),
+        Name:     data["name"],
+        Email:    data["email"],
+        Password: password,
+    }
+
+    collection := database.DB.Collection("users")
+    _, err := collection.InsertOne(context.Background(), user)
+    if err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "message": "Could not create user",
+        })
+    }
+
+    return c.JSON(user)
+}
+
+
 func Login(c *fiber.Ctx) error {
     var data map[string]string
     if err := c.BodyParser(&data); err != nil {
